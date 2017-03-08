a73997ea4c172857610cab90f9bfb017566f7ce3
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-03-08 22:45:08 +0530

    Removed unnecessary dependancies from Package.json.
    1. Introduces Github Repo URL
    2. Issues Link
    3. Readme link
    
    4. Removes node_modules access for ngx-modal
    
    Tracks #51

--- stack/stack-details/stack-details.module.ts
+++ stack/stack-details/stack-details.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
 import { StackDetailsComponent } from './stack-details.component';
-import { ModalModule } from '../../node_modules/ngx-modal';
+import { ModalModule } from 'ngx-modal';
 
 import { RecommenderModule } from '../recommender/recommender.module';
 import { StackComponentsModule } from '../stack-components/stack-components.module';
